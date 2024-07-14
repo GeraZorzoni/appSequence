8776a1a7e3a027269c38f314c8805907d0b23dc4
GeraZorzoni <gzorzoni@gmail.com>
2024-07-14 16:18:30 +0200

    add spinner on artist and compnies

--- src/app/songs/pages/list-page-artist/list-page-artist.component.ts
+++ src/app/songs/pages/list-page-artist/list-page-artist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Artist } from '../../interfaces/artist.interface';
 import { SongsService } from '../../services/songs.service';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'list-page-artist',
@@ -9,12 +10,22 @@ import { SongsService } from '../../services/songs.service';
 })
 export class ListPageArtistComponent implements OnInit {
   public artists: Artist[] = [];
+  public isLoading: boolean = true;
 
   constructor(private songsService: SongsService) {}
 
   ngOnInit(): void {
+    this.loadArtists();
+  }
+
+  loadArtists(): void {
+    this.isLoading = true;
     this.songsService
       .getArtist()
-      .subscribe((artists) => (this.artists = artists));
+      .pipe(delay(1000))
+      .subscribe((artists) => {
+        this.artists = artists;
+        this.isLoading = false;
+      });
   }
 }
--- src/app/songs/pages/list-page-companies/list-page-companies.component.ts
+++ src/app/songs/pages/list-page-companies/list-page-companies.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SongsService } from '../../services/songs.service';
 import { Company } from '../../interfaces/company.interface';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'list-page-companies',
@@ -9,12 +10,20 @@ import { Company } from '../../interfaces/company.interface';
 })
 export class ListPageCompaniesComponent implements OnInit {
   public companies: Company[] = [];
+  public isLoading: boolean = true;
 
   constructor(private songsService: SongsService) {}
 
   ngOnInit(): void {
+    this.loadCompanies();
+  }
+  loadCompanies(): void {
     this.songsService
       .getCompany()
-      .subscribe((companies) => (this.companies = companies));
+      .pipe(delay(1000))
+      .subscribe((companies) => {
+        this.companies = companies;
+        this.isLoading = false;
+      });
   }
 }
